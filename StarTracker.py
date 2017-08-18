6e3dc2af47bb8456d7a1b471e282ca548eed6831
Jean-Pierre de SOZA <ds.jaypee@gmail.com>
2017-08-18 09:54:11 +0200

    Avoid redirection with magic 204 trick

--- StarTracker.py
+++ StarTracker.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python3.5
 
-from flask import Flask
-from flask import request
-import RPi.GPIO as GPIO
+from flask import Flask, request, render_template
+#from flask_restplus import Api
 from StepperMotor import StepperMotor
 from SteppingMode import SteppingMode
 from MotorTask import MotorTask
 import threading
 
 app = Flask(__name__)
-GPIO.setmode(GPIO.BCM)
+#api = Api(app)
 stepper = StepperMotor([17,18,22,27],SteppingMode.HALF_STEP, 1, 500)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 @app.route("/step")
 def one_step():
     stepper.step()
-    return "Step: %s" % (stepper.pc)
+    return "{'%s':'%d'}" % ("pc", stepper.pc)
 
 @app.route("/start")
 def motor_start():
     stepper.start()
-    worker = getattr(MotorTask, 'run')
+    worker = getattr(MotorTask, "run")
     workThead = threading.Thread(target=worker, args=[stepper])
     workThead.start()
-    return "State: %s" % (stepper.state.name)
+    return "{'%s':'%s'}" % ("state", stepper.state.name), 204
 
 @app.route("/stop")
 def motor_stop():
     stepper.stop()
-    return "State: %s" % (stepper.state.name)
+    return "{'%s':'%s'}" % ("state", stepper.state.name), 204
 
 @app.route("/pause")
 def motor_pause():
     stepper.pause()
-    return "State: %s" % (stepper.state.name)
+    return "{'%s':'%s'}" % ("state", stepper.state.name), 204
 
 @app.route("/resume")
 def motor_resume():
@@ -45,28 +40,32 @@ def motor_resume():
     worker = getattr(MotorTask, 'run')
     workThead = threading.Thread(target=worker, args=[stepper])
     workThead.start()
-    return "State: %s" % (stepper.state.name)
+    return "{'%s':'%s'}" % ("state", stepper.state.name), 204
 
 @app.route("/faster")
 def increase_speed():
     stepper.faster()
-    return "Speed: %d" % (stepper.speed)
+    return "{'%s':'%d'}" % ("speed", stepper.speed), 204
 
 @app.route("/slower")
 def decrease_speed():
     stepper.slower()
-    return "Speed: %d" % (stepper.speed)
+    return "{'%s':'%d'}" % ("speed", stepper.speed), 204
 
 @app.route("/reverse")
 def motor_reverse():
     stepper.reverse()
-    return "Direction: %d" % (stepper.direction)
+    return "{'%s':'%d'}" % ("direction", stepper.direction), 204
 
-@app.route("/seconds/<seconds>", methods=["POST"])
+@app.route("/seconds/<seconds>", methods=["PUT","POST"])
 def motor_speed(seconds):
     assert seconds == request.view_args['seconds']
     stepper.set_speed(int(seconds))
-    return "Speed: %d" % (stepper.speed)
+    return "{'%s':'%d'}" % ("speed", stepper.speed), 204
+
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+        return render_template('%s.html' % page_name), 204
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
